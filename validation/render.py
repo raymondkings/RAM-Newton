07e092c377a18499d3f58300cf551aaa48f18a88
Julian Arkenau <julian.arkenau@tum.de>
2026-05-04 20:12:56 +0200

    do not require the reachable region to be set

--- validation/render.py
+++ validation/render.py
@@ -27,23 +27,24 @@ def render_scene(morph: Morphology, task: Task, port: int = 8080) -> None:
 
     # Reachable region
     region = task.reachable_region
-    builder.add_shape_box(
-        body=-1,
-        xform=wp.transform(
-            p=wp.vec3(
-                (region.x_min + region.x_max) / 2,
-                (region.y_min + region.y_max) / 2,
-                (region.z_min + region.z_max) / 2,
+    if region is not None:
+        builder.add_shape_box(
+            body=-1,
+            xform=wp.transform(
+                p=wp.vec3(
+                    (region.x_min + region.x_max) / 2,
+                    (region.y_min + region.y_max) / 2,
+                    (region.z_min + region.z_max) / 2,
+                ),
+                q=wp.quat_identity(),
             ),
-            q=wp.quat_identity(),
-        ),
-        hx=(region.x_max - region.x_min) / 2,
-        hy=(region.y_max - region.y_min) / 2,
-        hz=(region.z_max - region.z_min) / 2,
-        as_site=True,
-        color=wp.vec3(0.0, 0.8, 0.2),
-        label="reachable_region",
-    )
+            hx=(region.x_max - region.x_min) / 2,
+            hy=(region.y_max - region.y_min) / 2,
+            hz=(region.z_max - region.z_min) / 2,
+            as_site=True,
+            color=wp.vec3(0.0, 0.8, 0.2),
+            label="reachable_region",
+        )
 
     # Goal poses
     for i in range(task.goal_poses.shape[0]):
