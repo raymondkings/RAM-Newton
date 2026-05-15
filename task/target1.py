2fcdf2bc1280e29494789139a968a482caa597c2
LewisTZ <jiyaozhang601@qq.com>
2026-05-15 23:08:01 +0200

    added 2 more tasks

--- task/target1.py
+++ task/target1.py
@@ -1,19 +1,19 @@
 import torch
 
-
 def create_task():
     goals = torch.eye(4).unsqueeze(0).repeat(3, 1, 1)
 
     # Initial pose — in front of wall, pointing +x
     goals[0, :3, :3] = torch.tensor([[0.0, 0.0, 1.0], [0.0, 1.0, 0.0], [-1.0, 0.0, 0.0]])
     goals[0, :3, 3]  = torch.tensor([0.15, 0.0, 0.125])
-
-    # Back face — x_max center, pointing -x
-    goals[1, :3, :3] = torch.tensor([[0.0, 0.0, -1.0], [-0.0, 1.0, 0.0], [1.0, 0.0, 0.0]])
-    goals[1, :3, 3]  = torch.tensor([0.3175, 0.0, 0.125])
-
+   
     # Top — z_max center, pointing -z
-    goals[2, :3, :3] = torch.tensor([[-1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, -1.0]])
-    goals[2, :3, 3]  = torch.tensor([0.25, 0.0, 0.355])
+    goals[1, :3, :3] = torch.tensor([[-1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, -1.0]])
+    goals[1, :3, 3]  = torch.tensor([0.25, 0.0, 0.355])
+   
+    # Back face — x_max center, pointing -x
+    goals[2, :3, :3] = torch.tensor([[0.0, 0.0, -1.0], [-0.0, 1.0, 0.0], [1.0, 0.0, 0.0]])
+    goals[2, :3, 3]  = torch.tensor([0.3175, 0.0, 0.125])
+
 
     return goals
